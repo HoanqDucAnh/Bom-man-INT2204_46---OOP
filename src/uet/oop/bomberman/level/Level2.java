dad71f8485e8a8211f2736b25c1cd86b7e898d74
Viet Anh Hoang <hoangvietanh8203@gmail.com>
2022-10-31 18:01:46 +0700

    Update Level Map

--- src/uet/oop/bomberman/level/Level2.java
+++ src/uet/oop/bomberman/level/Level2.java
@@ -1,6 +1,35 @@
 package uet.oop.bomberman.level;
 
-public class Level2 {
+import uet.oop.bomberman.entities.Balloon;
+import uet.oop.bomberman.entities.Monster;
+import uet.oop.bomberman.graphics.Map;
+import uet.oop.bomberman.graphics.Sprite;
 
+
+import static uet.oop.bomberman.entities.Bomber.*;
+import static uet.oop.bomberman.BombermanGame.*;
+import static uet.oop.bomberman.BombermanGame.monsters;
+
+public class Level2 {
+    public Level2() {
+        stillObjects.clear();
+        monsters.clear();
+        items.clear();
+        new Map("res\\levels\\Level2.txt");
+        //new MonsterMap("res\\levels\\Level1.txt");
+        //bomberman.setLife(true);
+        currentSpeed = 1;
+        speedLeft = 1;
+        speedRight = 1;
+        speedDown = 1;
+        speedUp = 1;
+        Monster enemy = new Balloon(10,1, Sprite.balloom_left1.getFxImage());
+        Monster enemy1 = new Balloon(10,3, Sprite.balloom_left1.getFxImage());
+        monsters.add(enemy);
+        monsters.add(enemy1);
+        for (Monster monster : monsters) {
+            monster.setAlive(true);
+        }
+    }
 
 }
